5c398002edcfad67ff5a82c30a07683a0e6f57ae
ombandurkar <ombandurkar2410@gmail.com>
2023-12-26 17:38:55 +0530

    added fs module in blog.js routes

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 const Blog = require('../model/blog');
 const Comment = require('../model/comments');
 const { route } = require('./user');
